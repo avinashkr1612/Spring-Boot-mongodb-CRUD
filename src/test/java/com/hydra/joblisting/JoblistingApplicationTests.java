1dcb47ca88a25118c8fa566fcf889123e26ae4a4
avinashkr1612 <avinashkr1612@gmail.com>
2024-05-20 08:32:52 +0530

    initial commit

---
+++ src/test/java/com/hydra/joblisting/JoblistingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydra.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
